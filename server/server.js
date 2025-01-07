a4474c99b6401fd2c03d2c9c54c13dc5a0dc3561
Patrick Gephart <pmgephart@gmail.com>
2025-01-07 10:33:43 -0700

    initial app commit

---
+++ server/server.js
@@ -0,0 +1,51 @@
+const genres = require('./data/genres.json');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+
+/**
+ * API Call: get genre
+ */
+app.get('/api/genre/:key', (req, res) => {
+	const { key } = req.params;
+	const genre = genres.find((genre) => genre.key === key);
+
+	if(!genre) {
+		return res.status(404).json({
+			status: "failure",
+			error: "Genre not found"
+		});
+	}
+
+	res.status(200).json({
+		status: "success",
+		genre: genre
+	});
+});
+
+/**
+ * API Call: get genre bands
+ */
+app.get('/api/genre/:key/bands', (req, res) => {
+	const { key } = req.params;
+	const genre = genres.find((genre) => genre.key === key);
+
+	if(!genre) {
+		return res.status(404).json({
+			status: "failure",
+			error: "Genre not found"
+		});
+	}
+
+	res.status(200).json({
+		status: "success",
+		bands: genre.bands
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
